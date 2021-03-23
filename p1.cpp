4d99bead2ce43df7fbe5c4aaf93b9b1c8f3fe70c
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-03-23 09:59:29 +0000

    get the data, but still have to choose how to represent the data

--- p1.cpp
+++ p1.cpp
@@ -1,4 +1,24 @@
-#include <iostream>
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> graph;
+vector<vector<int>> graph_trans;
+
+void readGraph() {
+	int n, m;
+	scanf("%d,%d", &n, &m);
+	// Alocar o espaco para o grafo
+	graph = vector<vector<int>>(n, vector<int>());
+	graph_trans = vector<vector<int>>(n, vector<int>());
+	for (int i=0; i<m; i++) {
+		int x, y;
+		scanf("%d %d", &x, &y);
+		graph[x-1].push_back(y-1);
+		graph_trans[y-1].push_back(x-1);
+	}
+}
 
 int main(int argc, char *argv[]) {
 	return 0;
