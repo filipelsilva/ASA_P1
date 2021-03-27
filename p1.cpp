7e0f25d63baf4258ac288ce9aaf325ce94d4d082
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-03-27 11:58:36 +0000

    com o dag_trans podes ver quais são sources

--- p1.cpp
+++ p1.cpp
@@ -4,15 +4,18 @@
 using namespace std;
 
 vector<vector<int>> dag;
+vector<vector<int>> dag_trans;
 
 void parseDAG() {
 	int n, m;
 	scanf("%d %d", &n, &m);
 	dag = vector<vector<int>>(n, vector<int>());
+	dag_trans = vector<vector<int>>(n, vector<int>());
 	for (int i=0; i<m; i++) {
 		int x, y;
 		scanf("%d %d", &x, &y);
 		dag[x-1].push_back(y-1);
+		dag_trans[y-1].push_back(x-1);
 	}
 }
 
