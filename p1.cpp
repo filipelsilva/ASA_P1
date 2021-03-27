2131699c95a657ec9dd4fee0479008a46cbd9855
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-03-27 10:49:17 +0000

    .

--- p1.cpp
+++ p1.cpp
@@ -4,21 +4,21 @@
 using namespace std;
 
 vector<vector<int>> graph;
-vector<vector<int>> graph_trans;
 
 void readGraph() {
 	int n, m;
 	scanf("%d %d", &n, &m);
-	// Alocar o espaco para o grafo
 	graph = vector<vector<int>>(n, vector<int>());
-	graph_trans = vector<vector<int>>(n, vector<int>());
 	for (int i=0; i<m; i++) {
 		int x, y;
 		scanf("%d %d", &x, &y);
 		graph[x-1].push_back(y-1);
-		graph_trans[y-1].push_back(x-1);
 	}
 }
+
+void topologicalSort() {}
+
+void dfsoubfsnaosabemos() {}
 
 int main(int argc, char *argv[]) {
 	return 0;
