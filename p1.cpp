da2cd692a5de5bd58b320678457ed5db2069e4a0
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-03-30 15:54:12 +0100

    node class

--- p1.cpp
+++ p1.cpp
@@ -1,22 +1,64 @@
 #include <iostream>
+#include <cstdio>
 #include <vector>
+
+#define WHITE 0
+#define GREY 1
+#define BLACK 2
 
 using namespace std;
 
-vector<vector<int>> dag;
-vector<vector<int>> dag_trans;
+class Node {
+public:
+	int id;
+	int dist = 0;
+	int color = WHITE;
+	/* Node* father = NULL; */
+	/* vector<Node*> in = vector<Node*>(); */
+	/* vector<Node*> out = vector<Node*>(); */
+
+	Node(int id) {
+		this->id = id;
+	}
+
+	void changeDist(int dist) {
+		this->dist = dist;
+	}
+
+	/* void addFather(Node* n) { */
+	/* 	this->father = n; */
+	/* } */
+
+	/* void addIn(Node* n) { */
+	/* 	this->in.push_back(n); */
+	/* } */
+
+	/* void addOut(Node* n) { */
+	/* 	this->out.push_back(n); */
+	/* } */
+};
+
+vector<Node> dag;
+
+/* void DFS() { */
+/* 	for (int i = 0; i < dag.size(); i++) { */
+
+/* 	} */
+/* } */
 
 void parseDAG() {
 	int n, m;
 	cin >> n >> m;
-	dag = vector<vector<int>>(n, vector<int>());
-	dag_trans = vector<vector<int>>(n, vector<int>());
-	for (int i=0; i<m; i++) {
-		int x, y;
-		cin >> x >> y;
-		dag[x-1].push_back(y-1);
-		dag_trans[y-1].push_back(x-1);
+	dag.reserve(n);
+	for (int i = 0; i < n; i++) {
+		dag.push_back(Node(i));
 	}
+	/* for (int i = 0; i < m; i++) { */
+	/* 	int x, y; */
+	/* 	cin >> x >> y; */
+	/* 	dag[x-1].addOut(&dag[y-1]); */
+	/* 	dag[y-1].addIn(&dag[x-1]); */
+	/* } */
 }
 
 void topologicalSort() {}
@@ -24,5 +66,9 @@ void topologicalSort() {}
 void dfsoubfsnaosabemos() {}
 
 int main(int argc, char *argv[]) {
+	parseDAG();
+	for (Node n : dag) {
+		printf("%d\n", n.color);
+	}
 	return 0;
 }
