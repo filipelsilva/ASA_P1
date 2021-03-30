5050f3c7632d641ca7a974b4baee30baa9a37cee
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2021-03-30 21:59:11 +0100

    dfs still not working

--- p1.cpp
+++ p1.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-#include <cstdio>
 #include <vector>
+#include <cassert>
+
+#include <cstdio>
 
 #define WHITE 0
 #define GREY 1
@@ -13,6 +15,7 @@ public:
 	int id;
 	int dist = 0;
 	int color = WHITE;
+	int close;
 	Node* father = NULL;
 	vector<Node*> in = vector<Node*>();
 	vector<Node*> out = vector<Node*>();
@@ -20,51 +23,62 @@ public:
 	Node(int id) {
 		this->id = id;
 	}
-
-	void changeDist(int dist) {
-		this->dist = dist;
-	}
-
-	void addFather(Node* n) {
-		this->father = n;
-	}
-
-	void addIn(Node* n) {
-		this->in.push_back(n);
-	}
-
-	void addOut(Node* n) {
-		this->out.push_back(n);
-	}
 };
 
+int timer;
 vector<Node> dag;
 
-/* int DFS_Visit(Node n, time) { */
+int DFS_Visit(Node* n) {
+	assert(n->color == WHITE);
+	n->color = GREY;
+	timer++;
+	n->dist = timer;
+	for (Node* next : n->out) {
+		if (next->color == WHITE) {
+			next->father = n;
+			timer = DFS_Visit(next);
+		}
+	}
+	n->color = BLACK;
+	timer++;
+	n->close = timer;
+	return timer;
+}
 
-/* } */
+void DFS() {
+	for (Node n : dag) {
+		n.color = WHITE;
+		n.father = NULL;
+	}
+	timer = 0;
+	for (Node n : dag) {
+		if (n.color == WHITE) {
+			timer = DFS_Visit(&n);
+		}
+	}
+}
 
-/* void DFS() { */
-/* 	int time = 0; */
-/* 	for (Node n : dag) { */
-/* 		if (n.color == WHITE) { */
-/* 			time = DFS_Visit(n, time); */
-/* 		} */
-/* 	} */
-/* } */
+vector<Node*> findSources() {
+	vector<Node*> ret = vector<Node*>();
+	for (Node n : dag) {
+		if (n.in.empty()) {
+			ret.push_back(&n);
+		}
+	}
+	return ret;
+}
 
 void parseDAG() {
 	int n, m;
 	cin >> n >> m;
-	dag.reserve(n);
 	for (int i = 0; i < n; i++) {
 		dag.push_back(Node(i));
 	}
 	for (int i = 0; i < m; i++) {
 		int x, y;
 		cin >> x >> y;
-		dag[x-1].addOut(&dag[y-1]);
-		dag[y-1].addIn(&dag[x-1]);
+		dag[x-1].out.push_back(&dag[y-1]);
+		dag[y-1].in.push_back(&dag[x-1]);
 	}
 }
 
@@ -74,11 +88,13 @@ void dfsoubfsnaosabemos() {}
 
 int main(int argc, char *argv[]) {
 	parseDAG();
+	/* vector<Node*> sources = findSources(); */
+	DFS();
 	for (Node n : dag) {
 		cout << "Node " << n.id + 1 << endl;
-		for (Node* nn : n.out) {
-			printf("%d\n", nn->id + 1);
-		}
+		cout << "\tFather: " << n.id + 1 << endl;
+		cout << "\tEnd timer: " << n.close << endl;
+		cout << "\tColor: " << n.color << endl;
 	}
 	return 0;
 }
